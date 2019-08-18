85094a2a692d4ae87015f63a1914430efa87b87e
mihai-macaneata <themacaneata@gmail.com>
2019-08-18 20:53:07 +0300

    fixed path

--- client/src/router.js
+++ client/src/router.js
@@ -30,8 +30,8 @@ export default new Router({
       component: () => import(/* webpackChunkName: "article" */ './components/Article.vue')
     },
     {
-      path: '/appointment',
-      name: 'appointment',
+      path: '/programare',
+      name: 'programare',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
